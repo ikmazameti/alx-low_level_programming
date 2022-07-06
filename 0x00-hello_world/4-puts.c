f8546d18c0ee95ecd7c41671f1d028068a53c3f5
ikmazameti <ikmazameti@gmail.com>
2022-07-06 02:17:04 -0700

    using puts for printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,11 @@
 
 
 
-/**
+/*
  *
- * * main - prints the size of various types
- *
- * * Return: 0 if exited properly, non-zero otherwise
- *
- * */
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 
 int main(void)
 
