0199bf4091b57c7e665991bb65ea978b47553b51
ikmazameti <ikmazameti@gmail.com>
2022-07-06 10:02:08 -0700

    all

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-*main -> assign a random number to the variable n each time it is executed
-*and print the last digit of the number stored in the variable 
-*Return: Always 0 (Success)
-*/
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
 
 int main(void)
 {
