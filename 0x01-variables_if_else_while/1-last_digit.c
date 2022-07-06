94d567fd5edabab899c78924f27eed7cde45c4f3
ikmazameti <ikmazameti@gmail.com>
2022-07-06 08:03:38 -0700

    assign random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,7 +9,7 @@
  */
 
 int main(void)
-{	
+{
 	int n;
 	int x;
 
@@ -31,10 +31,9 @@ int main(void)
 
 	if (x < 6 && x != 0)
 	{
-		printf("and is less than 6 and not 0");	
+		printf("and is less than 6 and not 0");
 	}
 
 	printf("\n");
-	
 	return (0);
 }
