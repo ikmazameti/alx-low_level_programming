619f193cd9a95df6729879e16a805bb369a724a8
ikmazameti <ikmazameti@gmail.com>
2022-07-06 02:54:00 -0700

    with prinf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-/*
- * main - prints the size of various types
- * Return: 0 if exited properly, non-zero otherwise
+/**
+* main - prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
 */
-
 int main(void)
 {
-
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
@@ -19,5 +17,4 @@ int main(void)
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
-
 }
