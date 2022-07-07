c881e01a57afce9d42cda197c104e29f8da2c28e
ikmazameti <ikmazameti@gmail.com>
2022-07-07 23:10:17 +0000

    done all

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,6 +4,7 @@
  * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line
  * Return: 0
  */
+
 void print_most_numbers(void)
 {
 	char n = 0;
@@ -16,5 +17,6 @@ void print_most_numbers(void)
 		}
 		n++;
 	}
+	
 	_putchar('\n');
 }
