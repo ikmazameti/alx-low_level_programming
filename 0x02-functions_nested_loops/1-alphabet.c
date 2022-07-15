6524b544e04a0b0fa238ae9a2cd8ed9dcd378acd
Mawuli Azameti <60759789+ikmazameti@users.noreply.github.com>
2022-07-15 10:00:04 +0000

    rework
    completing task function and nested loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - in lower case
+ * Return 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - returns sum of parameters
+ * @a: int type
+ * @b: int typr
+ * Return: 0
+ */
+
+int add(int a, int b)
+
+{
+
+	return (a + b);
+
+}
