646afcb2459f38525b9f31e65a77a28a5daa55e5
ikmazameti <ikmazameti@gmail.com>
2022-07-19 12:02:40 +0000

    fixed styles for betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
   * _memset - Fill memory with a constant byte
   * @s: memory area to fill
@@ -8,6 +7,7 @@
   *
   * Return: the memory area filled
   */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
