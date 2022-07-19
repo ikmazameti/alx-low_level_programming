91bdf0a31c0e339246b1c6b2b289a9f4ec960107
Mawuli Azameti <60759789+ikmazameti@users.noreply.github.com>
2022-07-19 12:29:59 +0000

    fixed conflict

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,45 +10,18 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	/*return null if any one is NULL*/
-	if ((s == '\0') || (accept == '\0'))
-		return ('\0');
+	char *start = accept;
+
 	while (*s)
 	{
-		if (_strchr(accept, *s))
+		while (*accept)
 		{
-			return (s);
+			if (*accept == *s)
+				return (s);
+			accept++;
 		}
-		else
-		{
-			s++;
-		}
+		accept = start;
+		s++;
 	}
-	return ('\0');
-}
-
-/**
- *_strchr-locates a character in a string.
- *@s:pointer to a string.
- *@c:Character to locate within s.
- *
- *Return:pointer to the first occurrence of the character c in the string s
- *or Null if not found.
- */
-
-char *_strchr(char *s, char c)
-{
-	char *char_find = '\0';
-	/*handling null pointer*/
-	if (s != '\0')
-	{
-		do {
-			if (*s == (char)c)
-			{
-				char_find = s;
-				break;
-			}
-		} while (*s++);
-	}
-	return (char_find);
+	return (NULL);
 }
