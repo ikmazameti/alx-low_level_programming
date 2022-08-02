6e50b863093623ebfd6472c95c4813211898f17e
ikmazameti <ikmazameti@gmail.com>
2022-08-02 10:35:45 +0000

    added commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
-#ifndef DOG
-#define DOG
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - A new type representing dog.
+ *@name: The name of the dog.
+ *@age: The age of the dog.
+ *@owner: The name of the owner.
+*/
 
 struct dog
 {
